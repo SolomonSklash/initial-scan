c756199f88649251580f701dcc17a9e3db779d5a
bellma101 <bellma@0xfeed.io>
2019-05-06 15:38:12 -0500

    Fixed colors, added sleeps, improved ffuf and gobuster commands.

--- README.md
+++ README.md
@@ -5,21 +5,24 @@ A tool for performing an initial, information-gathering scan on a target URL bef
 Usage: initial-scan.sh [URL] [TARGET]
 
 A full URL including the scheme and/or port is required, as well as a target name. An output directory will be created with the target name along with a timestamp for each run of the script.
+Certain tools like Breacher won't work on file paths, like `index.html`, so make sure to use a directory path, like `/some/path/directory/`, as well as including a trailing slash, if you want to use Breacher.
 
-Example: `./initial-scan.sh https://www.example.com example`
+Examples: 
+* `./initial-scan.sh https://www.example.com example`
+* `./initial-scan.sh https://www.example.com/some/path/directory/ example`
 
 ## Installation
-All tools can be installed into the ~/tools directory (created as needed) by running initial-scan.sh install.
+All tools can be installed into the ~/tools directory (created as needed) by running `./initial-scan.sh install`. Existing tools will be removed and re-pulled from Github.
 
 ## Tools
 Initial Scan uses the following tools:
 * [nmap](https://nmap.org/) (Installed via Kali package)
 * [whatweb](https://www.morningstarsecurity.com/research/whatweb) (Installed via Kali package)
 * [nikto](https://cirt.net/nikto2) (Installed via Kali package)
+* [ffuf](https://github.com/ffuf/ffuf) (Binary downloaded from Github repo)
 * [gobuster](https://github.com/OJ/gobuster) (Installed via Kali package)
-* [sslscan](https://github.com/rbsec/sslscan/) (Installed via Kali package)
-* [ffuf](https://github.com/ffuf/ffuf) (Binary downloaded from git repo)
-* [bfac](https://github.com/mazen160/bfac) (clone git repo and add path to script)
-* [snallygaster](https://github.com/hannob/snallygaster) (clone git repo and add path to script)
+* [Breacher](https://github.com/s0md3v/Breacher) (Clone Github repo and add path to script)
+* [bfac](https://github.com/mazen160/bfac) (Clone Github repo and add path to script)
 * [wafw00f](https://github.com/EnableSecurity/wafw00f) (Installed via Kali package)
-* [Breacher](https://github.com/s0md3v/Breacher) (clone git repo and add path to script)
+* [sslscan](https://github.com/rbsec/sslscan/) (Installed via Kali package)
+* [twa](https://github.com/trailofbits/twa) (Clone Github repo and add path to script)
--- initial-scan.sh
+++ initial-scan.sh
@@ -21,6 +21,7 @@ TIME=$(date +%T);
 
 function installer() {
 		echo -e "$GREEN""[+] Installing nmap, whatweb, nikto, gobuster, wafw00f, and sslscan from repositories.""$NC";
+		sleep 1;
 		sudo apt install nmap whatweb nikto gobuster wafw00f sslscan;
 
 		echo -e "$GREEN""[+] Creating $HOME/tools directory for cloned tools.""$NC";
@@ -63,7 +64,7 @@ function installer() {
 		fi
 
 		echo -e "$BLUE""[i] Tools have been installed. Please run with arguments [URL] [TARGET].""$NC";
-		exit;
+		exit 0;
 }
 
 if [[ "$URL" == "install" ]]; then
@@ -141,19 +142,19 @@ function run_nmap() {
 
 		# Strip http/https and trailing path from URL
 		NMAP_URL=$(echo "$URL" | sed -e 's/^http\(\|s\):\/\///g' | sed -e 's/\/.*//');
-		echo -e "$GREEN""[*]$BLUE Running the following nmap command: sudo nmap $NMAP_URL -v -Pn -sV --reason --version-all --top-ports 1000 -oA $WORKING_DIR/nmap-top-1000 --stats-every 7s""$NC";
+		echo -e "$ORANGE""[*]$GREEN Running the following nmap command:$BLUE sudo nmap $NMAP_URL -v -Pn -sV --reason --version-all --top-ports 1000 -oA $WORKING_DIR/nmap-top-1000 --stats-every 7s""$NC";
 		sleep 1;
-		sudo nmap "$NMAP_URL" -v -Pn -sV --reason --version-all --top-ports 1000 -oA "$WORKING_DIR"/nmap-top-1000 --stats-every 7s;
+		nmap "$NMAP_URL" -v -Pn -sV --reason --version-all --top-ports 1000 -oA "$WORKING_DIR"/nmap-top-1000 --stats-every 7s;
 
-		echo -e "$GREEN""[*]$BLUE Running the following nmap command:  sudo nmap $URL -v -Pn -p 80,8080,443 --script http-apache-negotiation,http-apache-server-status,http-aspnet-debug,http-auth,http-auth-finder,http-config-backup,http-cors,http-cross-domain-policy,http-default-accounts,http-enum,http-errors,http-generator,http-iis-short-name-brute,http-iis-webdav-vuln,http-internal-ip-disclosure,,http-mcmp,http-method-tamper,http-methods,http-ntlm-info,http-open-proxy,http-open-redirect,http-passwd,http-php-version,http-phpself-xss,http-trace,http-traceroute,http-vuln-cve2012-1823,http-vuln-cve2015-1635 -oA $WORKING_DIR/nmap-http""$NC";
+		echo -e "$ORANGE""[*]$GREEN Running the following nmap command:$BLUE sudo nmap $URL -v -Pn -p 80,8080,443 --script http-apache-negotiation,http-apache-server-status,http-aspnet-debug,http-auth,http-auth-finder,http-config-backup,http-cors,http-cross-domain-policy,http-default-accounts,http-enum,http-errors,http-generator,http-iis-short-name-brute,http-iis-webdav-vuln,http-internal-ip-disclosure,,http-mcmp,http-method-tamper,http-methods,http-ntlm-info,http-open-proxy,http-open-redirect,http-passwd,http-php-version,http-phpself-xss,http-trace,http-traceroute,http-vuln-cve2012-1823,http-vuln-cve2015-1635 -oA $WORKING_DIR/nmap-http""$NC";
 		sleep 1;
-		sudo nmap "$NMAP_URL" -v -Pn -p 80,8080,443 --script http-apache-negotiation,http-apache-server-status,http-aspnet-debug,http-auth,http-auth-finder,http-config-backup,http-cors,http-cross-domain-policy,http-default-accounts,http-enum,http-errors,http-generator,http-iis-short-name-brute,http-iis-webdav-vuln,http-internal-ip-disclosure,,http-mcmp,http-method-tamper,http-methods,http-ntlm-info,http-open-proxy,http-open-redirect,http-passwd,http-php-version,http-phpself-xss,http-trace,http-traceroute,http-vuln-cve2012-1823,http-vuln-cve2015-1635 -oA "$WORKING_DIR"/nmap-http --stats-every 7s;
+		nmap "$NMAP_URL" -v -Pn -p 80,8080,443 --script http-apache-negotiation,http-apache-server-status,http-aspnet-debug,http-auth,http-auth-finder,http-config-backup,http-cors,http-cross-domain-policy,http-default-accounts,http-enum,http-errors,http-generator,http-iis-short-name-brute,http-iis-webdav-vuln,http-internal-ip-disclosure,,http-mcmp,http-method-tamper,http-methods,http-ntlm-info,http-open-proxy,http-open-redirect,http-passwd,http-php-version,http-phpself-xss,http-trace,http-traceroute,http-vuln-cve2012-1823,http-vuln-cve2015-1635 -oA "$WORKING_DIR"/nmap-http --stats-every 7s;
 }
 
 function run_whatweb() {
 		trap cancel SIGINT;
 
-		echo -e "$GREEN""[*]$BLUE Running whatweb with the following command: whatweb -v -a 3 $URL""$NC";
+		echo -e "$ORANGE""[*]$GREEN Running whatweb with the following command:$BLUE whatweb -v -a 3 $URL""$NC";
 		sleep 1;
 		whatweb -v -a 3 "$URL" | tee "$WORKING_DIR"/whatweb;
 }
@@ -161,7 +162,7 @@ function run_whatweb() {
 function run_nikto() {
 		trap cancel SIGINT;
 
-		echo -e "$GREEN""[*]$BLUE Running nikto with the following command: nikto -h $URL -output $WORKING_DIR/$TIME-nikto.txt""$NC";
+		echo -e "$ORANGE""[*]$GREEN Running nikto with the following command:$BLUE nikto -h $URL -output $WORKING_DIR/$TIME-nikto.txt""$NC";
 		sleep 1;
 		nikto -h "$URL" -output "$WORKING_DIR"/nikto.txt;
 }
@@ -169,33 +170,25 @@ function run_nikto() {
 function run_gobuster() {
 		trap cancel SIGINT;
 
-		echo -e "$GREEN""[*]$BLUE Running gobuster with the command: gobuster -u $URL -w big.txt -s '200,204,301,302,307,403,500' -e -o $WORKING_DIR/gobuster""$NC";
+		echo -e "$ORANGE""[*]$GREEN Running gobuster with the command:$BLUE gobuster -u $URL -w big.txt -s '200,204,301,302,307,403,500' -e -o $WORKING_DIR/gobuster""$NC";
 		sleep 1;
-		gobuster -u "$URL" -t 20 -w big.txt -s '200,204,301,302,307,403,500' -e -o "$WORKING_DIR"/gobuster;
+		gobuster -u "$URL" -t 20 -w big.txt -s '200,201,202,204,307,308,400,401,403,405,500,501,502,503' -to 3s -e -o -k "$WORKING_DIR"/gobuster;
 }
 
 function run_ffuf() {
 		trap cancel SIGINT;
 
-		echo -e "$GREEN""[*]$BLUE Running ffuf with the following command: ffuf -u $URL/FUZZ -w big.txt -k | tee $WORKING_DIR/ffuf-output.txt""$NC";
+		echo -e "$ORANGE""[*]$GREEN Running ffuf with the following command:$BLUE ffuf -u $URL/FUZZ -w big.txt -k -sf -se -fc 404 -mc all | tee $WORKING_DIR/ffuf-output.txt""$NC";
 		sleep 1;
-		"$FFUF" -u "$URL"/FUZZ -w big.txt -k | tee "$WORKING_DIR"/ffuf-output.txt;
+		"$FFUF" -u "$URL"/FUZZ -w big.txt -k -sf -se -fc 404 -mc all | tee "$WORKING_DIR"/ffuf-output.txt;
 }
 
 function run_bfac() {
 		trap cancel SIGINT;
 
-		echo -e "$GREEN""[*]$BLUE Running bfac with the following command: $BFAC -u $URL -xsc 404,400 -o $WORKING_DIR/bfac""$NC";
+		echo -e "$ORANGE""[*]$GREEN Running bfac with the following command:$BLUE $BFAC -u $URL -xsc 404,400 -o $WORKING_DIR/bfac""$NC";
 		sleep 1;
 		"$BFAC" -u "$URL" -xsc 404,400 -o "$WORKING_DIR"/bfac;
-}
-
-function run_snallygaster() {
-		trap cancel SIGINT;
-
-		echo -e "$GREEN""[*]$BLUE Running snallygaster with the following command: $SNALLYGASTER $URL -d | tee $WORKING_DIR/snallgaster""$NC";
-		sleep 1;
-		"$SNALLYGASTER" "$URL" -d | tee "$WORKING_DIR"/snallgaster;
 }
 
 function run_wafw00f() {
@@ -204,7 +197,7 @@ function run_wafw00f() {
 		# Get base URL with scheme
 		WAFW00F_URL="$(echo "$URL" | cut -d '/' -f 1)//$(echo "$URL" | cut -d '/' -f 3)";
 
-		echo -e "$GREEN""[*]$BLUE Running wafw00f with the following command: wafw00f $WAFW00F_URL -a | tee $WORKING_DIR/wafw00f""$NC";
+		echo -e "$ORANGE""[*]$GREEN Running wafw00f with the following command:$BLUE wafw00f $WAFW00F_URL -a | tee $WORKING_DIR/wafw00f""$NC";
 		sleep 1;
 		wafw00f "$WAFW00F_URL" -v -a 3 | tee "$WORKING_DIR"/wafw00f;
 }
@@ -212,22 +205,44 @@ function run_wafw00f() {
 function run_breacher() {
 		trap cancel SIGINT;
 
-		echo -e "$GREEN""[*]$BLUE Running breacher with the following command: python breacher.py -u $URL --fast""$NC";
-		cd "$HOME"/tools/Breacher;
-		python "$BREACHER" -u "$URL" --fast;
-		cd -;
+		if [[ "$URL" == "*/" ]]; then
+				echo -e "$ORANGE""[*]$GREEN Running breacher with the following command:$BLUE python breacher.py -u $URL --fast""$NC";
+				sleep 1;
+				cd "$HOME"/tools/Breacher;
+				python "$BREACHER" -u "$URL" --fast;
+				cd -;
+		else
+				# Get base URL with scheme
+				BREACHER_URL="$(echo "$URL" | cut -d '/' -f 1)//$(echo "$URL" | cut -d '/' -f 3)";
+
+				echo -e "$ORANGE""[*]$BLUE URL does not end with a path, which Breacher requires.""$NC";
+				echo -e "$ORANGE""[*]$BLUE If you want to run against a URL path, include a trailing slash.""$NC";
+				echo -e "$ORANGE""[*]$GREEN Running breacher against the base URL with the following command:$BLUE python breacher.py -u $BREACHER_URL --fast""$NC";
+				sleep 2;
+				cd "$HOME"/tools/Breacher;
+				python "$BREACHER" -u "$BREACHER_URL" --fast;
+				cd -;
+		fi
 }
 
 function run_sslscan() {
 		trap cancel SIGINT;
 
-		echo -e "$GREEN""[*]$BLUE Running sslscan with the following command: sslscan $URL""$NC";
-		sslscan "$URL";
+		# Strip http/https and trailing path from URL
+		SSLSCAN_URL=$(echo "$URL" | sed -e 's/^http\(\|s\):\/\///g' | sed -e 's/\/.*//');
+		echo -e "$ORANGE""[*]$GREEN Running sslscan with the following command:$BLUE sslscan $SSLSCAN_URL""$NC";
+		sleep 1;
+		sslscan "$SSLSCAN_URL";
 }
 
 function run_twa() {
-		echo -e "$GREEN""[*]$BLUE Running twa.sh with the following command: twa.sh $URL""$NC";
-		"$TWA" "$URL";
+		trap cancel SIGINT;
+
+		# Strip http/https and trailing path from URL
+		TWA_URL=$(echo "$URL" | sed -e 's/^http\(\|s\):\/\///g' | sed -e 's/\/.*//');
+		echo -e "$ORANGE""[*]$GREEN Running twa.sh with the following command:$BLUE twa.sh $TWA_URL""$NC";
+		sleep 1;
+		"$TWA" "$TWA_URL";
 }
 
 run_nmap;
@@ -237,7 +252,9 @@ run_ffuf;
 run_gobuster;
 run_breacher;
 run_bfac;
-# run_snallygaster;
 run_wafw00f;
 run_sslscan;
 run_twa;
+
+sleep 1;
+echo -e "$ORANGE""[*] Scan completed. See output at: ./$TARGET-$TIME""$NC";
